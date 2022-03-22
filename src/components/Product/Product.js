141587390eaa2b15c6af7b9def151ce0fb0e1518
arifvtti <arif.vtti@gmail.com>
2022-03-22 09:57:48 +0600

    evenhandler added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Product.css";
 const Product = (props) => {
+    const { addToOrderList } = props;
     const { img, name, price, ratings, seller } = props.product;
     return (
         <div className='product'>
@@ -16,8 +17,11 @@ const Product = (props) => {
                 </div>
 
             </div>
-            <button className='btn-cart'>
+            <button onClick={() => addToOrderList(props.product)} className='btn-cart'>
                 <p>Add to cart</p>
+                <svg xmlns="http://www.w3.org/2000/svg" className='cart-icon' fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                </svg>
             </button>
         </div>
     );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,11 +8,14 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProducts(data));
     }, [])
+    const addToOrderList = (product) => {
+        console.log(product);
+    }
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product => <Product key={product.id} product={product}></Product>)
+                    products.map(product => <Product key={product.id} product={product} addToOrderList={addToOrderList}></Product>)
                 }
             </div>
             <div className="order-container">
