5a858b6ed9bdd7fd289989361daebd70abcf54f0
arifvtti <arif.vtti@gmail.com>
2022-03-22 07:38:36 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from "../../images/Logo.svg";
+const Header = () => {
+    return (
+        <nav>
+            <img src="" alt="" />
+
+            <div>
+                <a href=""></a><a href=""></a><a href=""></a><a href=""></a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
