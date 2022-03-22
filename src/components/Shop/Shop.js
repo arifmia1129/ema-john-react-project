a5d2a5cfb0b83a9445933ed6e609ac97966108ae
arifvtti <arif.vtti@gmail.com>
2022-03-22 08:25:22 +0600

    container define

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import "./Shop.css";
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch("products.json")
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, [])
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h1>This is products container:{products.length}</h1>
+            </div>
+            <div className="order-container">
+                <h1>This is order container</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
