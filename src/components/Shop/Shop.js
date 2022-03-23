ccefb2e544c90a78c747c1caada0abb4cdfb3859
arifvtti <arif.vtti@gmail.com>
2022-03-23 11:11:05 +0600

    connect with localstorage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,6 +4,7 @@ import Product from '../Product/Product';
 import "./Shop.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashCan, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { addToDb, storedCart, deleteShoppingCart } from '../../utilities/fakedb';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [orders, setOrders] = useState([]);
@@ -11,12 +12,39 @@ const Shop = () => {
         fetch("products.json")
             .then(res => res.json())
             .then(data => setProducts(data));
-    }, [])
+    }, []);
+    useEffect(() => {
+        const stored = storedCart();
+        const savedCart = [];
+        for (const id in stored) {
+            const addedProducts = products.find(product => product.id === id);
+            if (addedProducts) {
+                const quantity = stored[id];
+                addedProducts.quantity = quantity;
+                savedCart.push(addedProducts);
+
+            }
+
+
+        }
+        setOrders(savedCart);
+    }, [products])
+
     const addToOrderList = (product) => {
         const totalOrders = [...orders, product];
         setOrders(totalOrders);
+        addToDb(product.id);
+        if (product.quantity === 0) {
+            product.quantity = 1;
+        }
     }
-    const totalPrice = calculate(orders, "price");
+    const totalQuantity = calculate(orders, "quantity");
+
+    let totalPrice = 0;
+    for (const order of orders) {
+        totalPrice = totalPrice + order.price * order.quantity;
+    }
+
     const totalShipping = calculate(orders, "shipping");
     const totalTax = (totalPrice * 10) / 100;
     const grandTotal = totalPrice + totalShipping + totalTax;
@@ -25,6 +53,7 @@ const Shop = () => {
     const clearCart = () => {
         const currentOrders = [];
         setOrders(currentOrders);
+        deleteShoppingCart();
     }
     return (
         <div className='shop-container'>
@@ -38,7 +67,7 @@ const Shop = () => {
                 <div className="order-info">
                     <h2>Order Summary</h2>
                     <br />
-                    <p>Selected item : {orders.length}</p>
+                    <p>Selected item : {totalQuantity}</p>
                     <p>Total price : {totalPrice}</p>
                     <p>Total shipping : {totalShipping}</p>
                     <p>Total tax : {totalTax}</p>
